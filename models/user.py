93e1f436417826e584c9bad8d18f43df9094ec42
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-29 02:14:22 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""All of the  holds class User"""
-import hashlib
+""" holds  all of the class User"""
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -17,16 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship(
-            "Place",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-        reviews = relationship(
-            "Review",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
@@ -35,13 +27,9 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            password_dt = kwargs.pop("password", None)
+            if password_dt:
+                password_md5 = hashlib.md5(password_dt.encode()).hexdigest()
+                setattr(self, "password", password_md5)
         super().__init__(*args, **kwargs)
-
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Sets an attribute of this class to a given value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                m = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
